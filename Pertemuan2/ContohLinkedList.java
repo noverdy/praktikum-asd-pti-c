c2c8aac03f3ed2b03c85797211a03ca95189e971
Noverdi Ramadhan <noverdy123@gmail.com>
2022-09-22 10:14:27 +0700

    pertemuan 2

---
+++ Pertemuan2/ContohLinkedList.java
@@ -0,0 +1,19 @@
+package Pertemuan2;
+
+import java.util.LinkedList;
+
+public class ContohLinkedList {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        System.out.println(list.isEmpty());
+        list.add(73); // nambahin di belakang
+        list.addLast(10); // nambahin di belakang
+        list.addFirst(90); // nambahin di depan
+        System.out.println(list); //ngeprint
+
+        list.removeLast(); // ngehapus di belakang
+        System.out.println(list);
+        list.removeFirst(); // ngehapus di depan
+        System.out.println(list);
+    }
+}
---
+++ Pertemuan2/Hackerrank1.java
@@ -0,0 +1,96 @@
+package Pertemuan2;/* Dilarang mengimpor library lain */
+import java.util.Scanner;
+
+public class Hackerrank1 {
+
+    public static void main(String[] args) {
+        /* Masukkan kode di sini. Selamat mengerjakan! */
+        LinkedList<String> list = new LinkedList<>();
+        list.addLast("Halo");
+        list.addLast("Hai");
+        list.addFirst("Test");
+
+        String hasil = list.toString();
+        hasil = hasil.replace(' ', '-');
+        System.out.println(hasil);
+    }
+}
+
+
+
+/* Jangan ubah kode di bawah ini */
+
+class LinkedList<T> {
+    private Node<T> head, tail;
+    private int size = 0;
+
+    public LinkedList() {
+        this.head = this.tail = null;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public void addFirst(T data) {
+        Node<T> node = new Node<>(data);
+        if (isEmpty()) {
+            this.head = this.tail = node;
+        }
+        else {
+            node.next = this.head;
+            this.head = node;
+        }
+        this.size++;
+    }
+
+    public void addLast(T data) {
+        Node<T> node = new Node<>(data);
+        if (isEmpty()) {
+            this.head = this.tail = node;
+        }
+        else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.size++;
+    }
+
+    public T getFirst() {
+        return this.head.data;
+    }
+
+    public T getLast() {
+        return this.tail.data;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        Node<T> temp = this.head;
+        while (temp != null) {
+            s.append(temp.data);
+            if (temp.next != null) s.append(" ");
+            temp = temp.next;
+        }
+        return s.toString();
+    }
+}
+
+class Node<T> {
+    public T data;
+    public Node<T> next;
+
+    public Node() {
+        this(null);
+    }
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+}
